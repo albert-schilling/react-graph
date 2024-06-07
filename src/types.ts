d449b0f215bb879f47182818c8c5928f61f8f7b0
Albert Schilling <59568178+albert-schilling@users.noreply.github.com>
2024-06-07 19:54:19 +0200

    RFC-001
    * Add RFC-001

---
+++ src/types.ts
@@ -0,0 +1,69 @@
+/**
+ * Represents a node in the graph.
+ */
+export interface INode {
+    /**
+     * The unique identifier of the node.
+     */
+    id: string
+
+    /**
+     * The label associated with the node.
+     */
+    label?: string
+
+    /**
+     * The edges connected to the node.
+     */
+    edges: IEdge[]
+
+    /**
+     * Additional metadata associated with the node.
+     */
+    meta?: Record<string, any>
+}
+
+/**
+ * Represents an edge between two nodes in the graph.
+ */
+export interface IEdge {
+    /**
+     * The first node of the edge.
+     */
+    a: INode
+
+    /**
+     * The second node of the edge.
+     */
+    b: INode
+
+    /**
+     * Additional metadata associated with the edge.
+     */
+    meta?: Record<string, any>
+}
+
+/**
+ * Represents a location in the graph.
+ */
+export interface ILocation {
+    /**
+     * The unique identifier of the location.
+     */
+    id: string
+
+    /**
+     * The position of the location, represented by a node.
+     */
+    position: INode
+
+    /**
+     * The path of edges leading to the location.
+     */
+    path: IEdge[]
+}
+
+/**
+ * Represents a selection of nodes in the graph.
+ */
+export type TSelection = INode[]
