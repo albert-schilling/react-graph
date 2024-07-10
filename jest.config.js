41ee87ea66c07cb171ce28c614b69700847e1296
albert-schilling <albertschilling@gmx.net>
2024-07-10 22:59:22 +0200

    Add GitHub Action

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,9 @@ module.exports = {
     '^@contexts/(.*)$': path.resolve(__dirname, 'src/contexts/$1'),
     '^@helpers/(.*)$': path.resolve(__dirname, 'src/helpers/$1'),
     '^@models/(.*)$': path.resolve(__dirname, 'src/models/$1')
-  }
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/tests/**/*.{js,jsx,ts,tsx}'
+  ]
 }
