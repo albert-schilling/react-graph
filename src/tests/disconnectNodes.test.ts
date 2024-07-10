21ae3edd83c70f7941b0d4f9f72d93cda8a0a3fd
albert-schilling <albertschilling@gmx.net>
2024-07-10 23:24:08 +0200

    Add disconnectNodes test

--- src/tests/disconnectNodes.test.ts
+++ src/tests/disconnectNodes.test.ts
@@ -1,39 +1,39 @@
 import ReactGraph from '@models/ReactGraph'
 
 describe('ReactGraph', () => {
-  describe('connectNodes', () => {
-    it('should connect two nodes', () => {
+  describe('disconnectNodes', () => {
+    it('should disconnect two connected nodes', () => {
       // Arrange
       const graph = new ReactGraph()
       const node1 = graph.addNode('Node 1')
       const node2 = graph.addNode('Node 2')
-
-      // Act
       graph.connectNodes(node1, node2)
 
-      // Assert
       expect(graph.getEdges()).toHaveLength(1)
-      expect(graph.getEdges()[0].a).toBe(node1)
-      expect(graph.getEdges()[0].b).toBe(node2)
-    })
-
-    it('should not connect the same node', () => {
-      // Arrange
-      const graph = new ReactGraph()
-      const node1 = graph.addNode('Node 1')
 
       // Act
-      try {
-        graph.connectNodes(node1, node1)
-      } catch (error) {
-        expect(error).toBeInstanceOf(Error)
-      }
+      graph.disconnectNodes(node1, node2)
 
       // Assert
       expect(graph.getEdges()).toHaveLength(0)
     })
 
-    it('should not connect nodes that are already connected', () => {
+    it('should not disconnect nodes that are not connected', () => {
+      // Arrange
+      const graph = new ReactGraph()
+      const node1 = graph.addNode('Node 1')
+      const node2 = graph.addNode('Node 2')
+
+      expect(graph.getEdges()).toHaveLength(0)
+
+      // Act
+      graph.disconnectNodes(node1, node2)
+
+      // Assert
+      expect(graph.getEdges()).toHaveLength(0)
+    })
+
+    it('should not disconnect the same node', () => {
       // Arrange
       const graph = new ReactGraph()
       const node1 = graph.addNode('Node 1')
@@ -43,7 +43,7 @@ describe('ReactGraph', () => {
       expect(graph.getEdges()).toHaveLength(1)
 
       // Act
-      graph.connectNodes(node1, node2)
+      graph.disconnectNodes(node1, node1)
 
       // Assert
       expect(graph.getEdges()).toHaveLength(1)
